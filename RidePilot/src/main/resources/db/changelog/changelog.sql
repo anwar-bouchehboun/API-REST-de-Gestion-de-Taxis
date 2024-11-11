be10cf0e9509e6bb13d35260c71557afdc87da96
moha.hach.9 <moha.hach.9@gmail.com>
2024-11-11 13:03:25 +0000

    setup project with the necessary dependecies [REST-CONF ENV - DB MIGRATION]

---
+++ RidePilot/src/main/resources/db/changelog/changelog.sql
@@ -0,0 +1,5 @@
+-- Add some database changes, e.g., creating tables, altering schemas, etc.
+CREATE TABLE example_table (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
---
+++ RidePilot/src/test/java/com/YC/RidePilot/RidePilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YC.RidePilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidePilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
